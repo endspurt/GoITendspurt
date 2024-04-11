f856b02f8946a8951fac7df97e1d58b810daf1e9
endspurt <alemannien@gmail.com>
2024-04-11 18:32:12 +0200

    noch dazu

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print ("Hallo GoIT")
-print ("Tschuess")
+#print ("Hallo GoIT")
+#print ("Tschuess")
+
+name = input ("enter kake: ")
+age = input ("enter doppel kake")
+
+#formated string
+message = f"good morning Mr{name}"
+welcome = f" You are {age*2}"
+
+print (message)
+print (age)
