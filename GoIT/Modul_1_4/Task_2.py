f3851782f011b57d27c9bad501e71c9753a1d0a0
endspurt <alemannien@gmail.com>
2024-04-20 10:23:18 +0200

    kommentar war korrigiert

--- GoIT/Modul_1_4/Task_2.py
+++ GoIT/Modul_1_4/Task_2.py
@@ -3,7 +3,7 @@ import random
 def get_numbers_ticket(min, max, quantity):
     # Validate input parameters
     if min < 1 or max > 1000 or min > max or quantity < 1 or quantity > (max - min + 1):
-        print("Invalid input parameters. Ensure that 1 <= min <= max <= 1000 and 1 <= quantity <= (max - min + 1).")
+        print("Invalid input parameters.")
         return []
 
     # Generate a list of unique random numbers using random.sample
