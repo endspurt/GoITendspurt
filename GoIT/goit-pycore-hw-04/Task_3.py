7c10cf2f39f3d7fd3e6372c5ca7ca343bbd582ce
endspurt <alemannien@gmail.com>
2024-04-27 18:15:40 +0200

    Versuch_zwei

--- GoIT/goit-pycore-hw-04/Task_3.py
+++ GoIT/goit-pycore-hw-04/Task_3.py
@@ -1,30 +1,34 @@
 import sys
 from pathlib import Path
-from colorama import Fore, Style
+from colorama import Fore, Style, init
 
-def print_directory_structure(path, prefix=''):
+init()  # Initialisiert colorama, um die Farben auf der Konsole zu aktivieren
+
+def print_directory_structure(path, indent=''):
     path = Path(path)
-    entries = list(path.iterdir())
-
-    for entry in entries:
-        connector = "┣" if entry != entries[-1] else "┗"
-        if entry.is_dir():
-            # Ausgabe für ein Verzeichnis
-            print(f"{prefix}{connector} {Fore.BLUE}{entry.name}{Style.RESET_ALL}")
-            # Rekursiver Aufruf für das Unterverzeichnis
-            print_directory_structure(entry, prefix=prefix + " ┃ ")
-        else:
-            # Ausgabe für eine Datei
-            print(f"{prefix}{connector} {Fore.GREEN}{entry.name}{Style.RESET_ALL}")
+    if path.is_dir():
+        entries = list(path.iterdir())
+        entries.sort(key=lambda x: (x.is_file(), x.name))
+        
+        for entry in entries:
+            if entry.is_dir():
+                # Ausgabe für ein Verzeichnis
+                print(f"{indent}{Fore.BLUE}{entry.name}{Style.RESET_ALL}")
+                # Rekursiver Aufruf für das Unterverzeichnis mit erhöhter Einrückung
+                print_directory_structure(entry, indent + '    ')
+            else:
+                # Ausgabe für eine Datei
+                print(f"{indent}{Fore.GREEN}{entry.name}{Style.RESET_ALL}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Bitte geben Sie den Pfad zum Verzeichnis als Argument an.")
+        print("Verwendung: python hw03.py <Pfad>")
         sys.exit(1)
-    
+
     directory_path = sys.argv[1]
     if not Path(directory_path).is_dir():
         print(f"Der Pfad {directory_path} existiert nicht oder ist kein Verzeichnis.")
         sys.exit(1)
 
     print_directory_structure(directory_path)
+
