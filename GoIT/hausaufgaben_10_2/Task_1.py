8170be312c0b9b647c3180b3bb3a72ee97c1390b
endspurt <alemannien@gmail.com>
2024-05-13 09:42:07 +0200

    eins

---
+++ GoIT/hausaufgaben_10_2/Task_1.py
@@ -0,0 +1,13 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# Beispiel zur Verwendung der Klasse
+point = Point(5, 10)
+print(point.x)  # Gibt 5 aus
+print(point.y)  # Gibt 10 aus
+
+'''Erstelle eine Klasse Point, die für die Darstellung eines geometrischen Punktes in der Ebene zuständig ist.
+
+Implementiere im Konstruktor __init__ die Initialisierung von zwei Attributen: die Koordinate x und die Koordinate y.'''
